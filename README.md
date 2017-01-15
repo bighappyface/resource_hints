9ded6f6f8a59a80fb90d4010577f6c8889409e59
bighappyface <anomalous20@gmail.com>
2017-01-15 14:40:00 -0600

    Add form support for all hint types Add permissions Add menu link Add README Add ResourceHintsOutputTest

---
+++ README.md
@@ -0,0 +1,21 @@
+# Resource Hints
+
+Configure resource hints for better user agent performance
+
+https://www.w3.org/TR/resource-hints/
+
+## Instructions
+
+Unpack in the *modules* folder (currently in the root of your Drupal 8 installation) and enable in `/admin/modules`.
+
+Then, visit `/admin/config/development/resource-hints` and enter your own set of configurations for the various resource hint types.
+
+## Helpful resources
+
+https://www.w3.org/TR/resource-hints/
+https://www.igvita.com/2015/08/17/eliminating-roundtrips-with-preconnect/
+https://css-tricks.com/prefetching-preloading-prebrowsing/
+https://medium.com/@luisvieira_gmr/html5-prefetch-1e54f6dda15d#.liri85j7v
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Link_prefetching_FAQ
+https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types
+https://www.w3.org/TR/html4/types.html#type-links
--- src/Form/ResourceHintsSettingsForm.php
+++ src/Form/ResourceHintsSettingsForm.php
@@ -6,9 +6,15 @@ use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Configure resource_hints settings for this site.
+ * Configure resource hints for this site.
  */
 class ResourceHintsSettingsForm extends ConfigFormBase {
+
+  const OUTPUT_LINK_ELEMENT = 0;
+  const OUTPUT_LINK_HEADER = 1;
+  const DNS_PREFETCH_HTTP_ONLY = 2;
+  const DNS_PREFETCH_HTTP_AND_HTTPS = 3;
+  const DNS_PREFETCH_DISABLED = 4;
 
   /**
    * {@inheritdoc}
@@ -32,11 +38,109 @@ class ResourceHintsSettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('resource_hints.settings');
 
-    $form['resource_hints_preconnect_resources'] = array(
-      '#type' => 'textfield',
-      '#title' => $this->t('Preconnect Resources'),
+    $form['dns_prefetch'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('DNS Prefetch'),
+    ];
+
+    $form['dns_prefetch']['dns_prefetch_output'] = [
+      '#type' => 'select',
+      '#title' => t('Output type'),
+      '#options' => [
+        self::OUTPUT_LINK_HEADER => t('Link Header'),
+        self::OUTPUT_LINK_ELEMENT => t('Link Element'),
+      ],
+      '#default_value' => $config->get('dns_prefetch_output'),
+      '#description' => t('Resource hints can be output as an HTTP Link header or HTML link element'),
+    ];
+
+    $form['dns_prefetch']['dns_prefetch_resources'] = [
+      '#type' => 'textarea',
+      '#default_value' => $config->get('dns_prefetch_resources'),
+      '#title' => $this->t('Resources'),
+      '#description' => t('The DNS resources you wish to be prefetched. Enter one resource per line.'),
+    ];
+
+    $form['dns_prefetch']['dns_prefetch_protocol_control'] = [
+      '#type' => 'select',
+      '#title' => t('DNS Prefetch Protocol Control'),
+      '#options' => [
+        self::DNS_PREFETCH_HTTP_ONLY => t('HTTP Only'),
+        self::DNS_PREFETCH_HTTP_AND_HTTPS => t('HTTP and HTTPS'),
+        self::DNS_PREFETCH_DISABLED => t('Disabled'),
+      ],
+      '#default_value' => $config->get('dns_prefetch_protocol_control'),
+      '#description' => t('By default browsers will not use DNS prefetching when a page is served via HTTPS, you must explicitly enable prefetching for HTTPS. Disabling prefetching will prevent browsers using prefetching and any inline attempts to enable it will be ignored.'),
+    ];
+
+    $form['preconnect'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Preconnect'),
+    ];
+
+    $form['preconnect']['preconnect_output'] = [
+      '#type' => 'select',
+      '#title' => t('Output type'),
+      '#options' => [
+        self::OUTPUT_LINK_HEADER => t('Link Header'),
+        self::OUTPUT_LINK_ELEMENT => t('Link Element'),
+      ],
+      '#default_value' => $config->get('preconnect_output'),
+      '#description' => t('Resource hints can be output as an HTTP Link header or HTML link element'),
+    ];
+
+    $form['preconnect']['preconnect_resources'] = [
+      '#type' => 'textarea',
       '#default_value' => $config->get('preconnect_resources'),
-    );
+      '#title' => $this->t('Resources'),
+      '#description' => t('The resources you wish to be preconnected. Enter one resource per line.'),
+    ];
+
+    $form['prefetch'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Prefetch'),
+    ];
+
+    $form['prefetch']['prefetch_output'] = [
+      '#type' => 'select',
+      '#title' => t('Output type'),
+      '#options' => [
+        self::OUTPUT_LINK_HEADER => t('Link Header'),
+        self::OUTPUT_LINK_ELEMENT => t('Link Element'),
+      ],
+      '#default_value' => $config->get('prefetch_output'),
+      '#description' => t('Resource hints can be output as an HTTP Link header or HTML link element'),
+    ];
+
+    $form['prefetch']['prefetch_resources'] = [
+      '#type' => 'textarea',
+      '#default_value' => $config->get('prefetch_resources'),
+      '#title' => $this->t('Resources'),
+      '#description' => t('The resources you wish to be prefetched. Enter one resource per line.'),
+    ];
+
+    $form['prerender'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Prerender'),
+    ];
+
+    $form['prerender']['prerender_output'] = [
+      '#type' => 'select',
+      '#title' => t('Output type'),
+      '#options' => [
+        self::OUTPUT_LINK_HEADER => t('Link Header'),
+        self::OUTPUT_LINK_ELEMENT => t('Link Element'),
+      ],
+      '#default_value' => $config->get('prerender_output'),
+      '#description' => t('Resource hints can be output as an HTTP Link header or HTML link element'),
+    ];
+
+    $form['prerender']['prerender_resources'] = [
+      '#type' => 'textarea',
+      '#default_value' => $config->get('prerender_resources'),
+      '#title' => $this->t('Resources'),
+      '#description' => t('The resources you wish to be prerendered. Enter one resource per line.'),
+    ];
 
     return parent::buildForm($form, $form_state);
   }
@@ -46,7 +150,15 @@ class ResourceHintsSettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = \Drupal::service('config.factory')->getEditable('resource_hints.settings');
-    $config->set('preconnect_resources', $form_state->getValue('resource_hints_preconnect_resources'))
+    $config->set('dns_prefetch_resources', $form_state->getValue('dns_prefetch_resources'))
+      ->set('dns_prefetch_output', $form_state->getValue('dns_prefetch_output'))
+      ->set('dns_prefetch_protocol_control', $form_state->getValue('dns_prefetch_protocol_control'))
+      ->set('preconnect_resources', $form_state->getValue('preconnect_resources'))
+      ->set('preconnect_output', $form_state->getValue('preconnect_output'))
+      ->set('prefetch_resources', $form_state->getValue('prefetch_resources'))
+      ->set('prefetch_output', $form_state->getValue('prefetch_output'))
+      ->set('prerender_resources', $form_state->getValue('prerender_resources'))
+      ->set('prerender_output', $form_state->getValue('prerender_output'))
       ->save();
 
     parent::submitForm($form, $form_state);
